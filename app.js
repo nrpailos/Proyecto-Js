cca6b408d1e3f2e766030704401f8d93404d5918
nrpailos <nr.pailos@gmail.com>
2022-04-05 16:50:04 -0300

    final proyecto js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const total = document.getElementById('total')
 
 
 //FUNCIONES
+
 let crearItem = (gasto, descripcion, date) => {
     let item = {
         gasto: parseInt(gasto),
@@ -40,126 +41,10 @@ sumarGastos = () => {
     if (total == 0) {
         Swal.fire('No se ingresaron gastos mensuales')
     } else {
-        totalGastos.innerHTML += `<div class="alert alert-danger" role="alert">
-        <div class="container">
-            <div class="moneda">
-                <select id="moneda-uno">
-                    <option value="AED">AED</option>
-                    <option value="ARS" selected>ARS</option>
-                    <option value="AUD">AUD</option>
-                    <option value="BGN">BGN</option>
-                    <option value="BRL">BRL</option>
-                    <option value="BSD">BSD</option>
-                    <option value="CAD">CAD</option>
-                    <option value="CHF">CHF</option>
-                    <option value="CLP">CLP</option>
-                    <option value="CNY">CNY</option>
-                    <option value="COP">COP</option>
-                    <option value="CZK">CZK</option>
-                    <option value="DKK">DKK</option>
-                    <option value="DOP">DOP</option>
-                    <option value="EGP">EGP</option>
-                    <option value="EUR">EUR</option>
-                    <option value="FJD">FJD</option>
-                    <option value="GBP">GBP</option>
-                    <option value="GTQ">GTQ</option>
-                    <option value="HKD">HKD</option>
-                    <option value="HRK">HRK</option>
-                    <option value="HUF">HUF</option>
-                    <option value="IDR">IDR</option>
-                    <option value="ILS">ILS</option>
-                    <option value="INR">INR</option>
-                    <option value="ISK">ISK</option>
-                    <option value="JPY">JPY</option>
-                    <option value="KRW">KRW</option>
-                    <option value="KZT">KZT</option>
-                    <option value="MXN">MXN</option>
-                    <option value="MYR">MYR</option>
-                    <option value="NOK">NOK</option>
-                    <option value="NZD">NZD</option>
-                    <option value="PAB">PAB</option>
-                    <option value="PEN">PEN</option>
-                    <option value="PHP">PHP</option>
-                    <option value="PKR">PKR</option>
-                    <option value="PLN">PLN</option>
-                    <option value="PYG">PYG</option>
-                    <option value="RON">RON</option>
-                    <option value="RUB">RUB</option>
-                    <option value="SAR">SAR</option>
-                    <option value="SEK">SEK</option>
-                    <option value="SGD">SGD</option>
-                    <option value="THB">THB</option>
-                    <option value="TRY">TRY</option>
-                    <option value="TWD">TWD</option>
-                    <option value="UAH">UAH</option>
-                    <option value="USD">USD</option>
-                    <option value="UYU">UYU</option>
-                    <option value="VND">VND</option>
-                    <option value="ZAR">ZAR</option>
-                </select>
-                <input type="number" id="cantidad-uno" placeholder="0" value="${total}">
-            </div>
-            <div class="cambio" id="cambio"></div>
-        </div>
-        <div class="moneda">
-            <select id="moneda-dos">
-                    <option value="AED">AED</option>
-                    <option value="ARS">ARS</option>
-                    <option value="AUD">AUD</option>
-                    <option value="BGN">BGN</option>
-                    <option value="BRL">BRL</option>
-                    <option value="BSD">BSD</option>
-                    <option value="CAD">CAD</option>
-                    <option value="CHF">CHF</option>
-                    <option value="CLP">CLP</option>
-                    <option value="CNY">CNY</option>
-                    <option value="COP">COP</option>
-                    <option value="CZK">CZK</option>
-                    <option value="DKK">DKK</option>
-                    <option value="DOP">DOP</option>
-                    <option value="EGP">EGP</option>
-                    <option value="EUR">EUR</option>
-                    <option value="FJD">FJD</option>
-                    <option value="GBP">GBP</option>
-                    <option value="GTQ">GTQ</option>
-                    <option value="HKD">HKD</option>
-                    <option value="HRK">HRK</option>
-                    <option value="HUF">HUF</option>
-                    <option value="IDR">IDR</option>
-                    <option value="ILS">ILS</option>
-                    <option value="INR">INR</option>
-                    <option value="ISK">ISK</option>
-                    <option value="JPY">JPY</option>
-                    <option value="KRW">KRW</option>
-                    <option value="KZT">KZT</option>
-                    <option value="MXN">MXN</option>
-                    <option value="MYR">MYR</option>
-                    <option value="NOK">NOK</option>
-                    <option value="NZD">NZD</option>
-                    <option value="PAB">PAB</option>
-                    <option value="PEN">PEN</option>
-                    <option value="PHP">PHP</option>
-                    <option value="PKR">PKR</option>
-                    <option value="PLN">PLN</option>
-                    <option value="PYG">PYG</option>
-                    <option value="RON">RON</option>
-                    <option value="RUB">RUB</option>
-                    <option value="SAR">SAR</option>
-                    <option value="SEK">SEK</option>
-                    <option value="SGD">SGD</option>
-                    <option value="THB">THB</option>
-                    <option value="TRY">TRY</option>
-                    <option value="TWD">TWD</option>
-                    <option value="UAH">UAH</option>
-                    <option value="USD" selected>USD</option>
-                    <option value="UYU">UYU</option>
-                    <option value="VND">VND</option>
-                    <option value="ZAR">ZAR</option>
-                </select>
-            <input type="number" id="cantidad-dos" placeholder="0">
-        </div>
-    </div>`
-            // totalGastos.innerHTML += `<div class="alert alert-danger" role="alert"><i class="material-icons float-left mr-2"></i><b> TOTAL GASTOS DEL MES: ${total}</b><span class="float-right"></span></div>`
+
+        cantidadUno.value = total;
+        calculate()
+
     }
 
 }
@@ -183,10 +68,10 @@ let mostrarGastos = () => {
     } else {
 
         arrayGastos.forEach(element => {
-            listaGastosUI.innerHTML += `<div class="alert alert-danger" role="alert">
-            <i class="material-icons float-left mr-2"></i>
+            listaGastosUI.innerHTML += `<div class="alert alert-dark" role="alert">
+            <i class="material-icons float-left mr-3"></i>
             <b>${element.gasto}</b> - ${element.descripcion} - ${element.date} 
-            <span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete</i></span></div>`
+            <span class="float-right"><i class="material-icons">delete</i></span></div>`
         });
     }
 }
@@ -206,10 +91,6 @@ let eliminar = (gasto) => {
     guardarGasto();
 }
 
-function conversor() {
-    const monedaUno = totalGastos.value
-
-}
 //FUNCION DEL CONVERSOR
 function calculate() {
     const moneda_uno = monedaUno.value;
@@ -250,7 +131,7 @@ listaGastosUI.addEventListener('click', (e) => {
     e.preventDefault();
 
 
-    if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete') {
+    if (e.target.innerHTML === 'delete') {
 
         let texto = e.path[2].childNodes[1].innerHTML;
         if (e.target.innerHTML === 'delete') {
@@ -265,6 +146,6 @@ listaGastosUI.addEventListener('click', (e) => {
 monedaUno.addEventListener('change', calculate);
 cantidadUno.addEventListener('input', calculate);
 monedaDos.addEventListener('change', calculate);
-cantidadUno.addEventListener('input', calculate);
+
 
 calculate();
